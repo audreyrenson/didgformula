b60f940701df3452cfcd59d1f7638d61963d5aeb
Audrey <audrey.o.renson@gmail.com>
2021-11-11 16:23:15 -0500

    ability to generate aggregate binomial data (i.e. binomial with n>1)

--- R/generate_data.R
+++ R/generate_data.R
@@ -5,8 +5,9 @@
 #' @param Beta list of length 4. Output of generate_parameters().
 #' @param potential_outcomes logical. Should outcomes and covariates be generated with exposure set to 0 at all times?
 #' @param ylink chr. One of "rnorm_identity", "rbinom_logit", or "rbinom_logit_hazard".
+#' @param binomial_n int length N. Defaults to all 1's. If ylink is rbinom_logit, you can optionally pass a vector of group sizes to generate aggregate binomial data. In this case, treatments and covariates will be constant at the group level for a given time period.
 #'
-#' @return Data frame with N rows and (Tt+1)*3 + 2 columns - 'uid' is a unique identifier, 'U0' is an 'unmeasured' baseline covariate, L{t},A{t},Y{t} are covariates, exposures, and outcomes, respectively.
+#' @return Data frame with N rows and (Tt+1)*3 + 2 columns - 'uid' is a unique identifier, 'U0' is an 'unmeasured' baseline covariate, L{t},A{t},Y{t} are covariates, exposures, and outcomes, respectively. If binomial_n != 1, an additional column binomial_n is also included.
 #' @export
 #'
 #' @examples
@@ -14,7 +15,8 @@ generate_data <- function(N,
                           Tt,
                           Beta,
                           potential_outcomes=FALSE,
-                          ylink = "rnorm_identity"){
+                          ylink = "rnorm_identity",
+                          binomial_n=1){
 
   df = data.frame(uid       = seq_len(N),
                   intercept = 1,
@@ -38,7 +40,9 @@ generate_data <- function(N,
     }
   }
 
-  df = cbind(df, simulate_y(df=df, Tt=Tt, vars_Y=vars_Y, Beta_Y=Beta$Y, ylink=ylink))
+  df = cbind(df, simulate_y(df=df, Tt=Tt, vars_Y=vars_Y, Beta_Y=Beta$Y, ylink=ylink, binomial_n=binomial_n))
+
+  if(length(binomial_n) > 1) df$binomial_n = binomial_n
 
   return( df[, -grep( c('intercept|zeros|U0times'), names(df))] )
 }
@@ -47,7 +51,7 @@ generate_data <- function(N,
 rbinom_logit <- function(X, Beta, N, n=1) rbinom(n=N, p=plogis(as.matrix(X) %*% Beta), size=n)
 
 simulate_y <- function(df, Tt, vars_Y, Beta_Y,
-                       ylink='rnorm_identity', binomial_n=1, ...) {
+                       ylink='rnorm_identity', binomial_n, ...) {
 
   eta_ti = sapply(0:Tt, function(t) as.matrix(df[vars_Y(t)]) %*% Beta_Y[t+1, ])
 
--- tests/testthat/test-generate_data.R
+++ tests/testthat/test-generate_data.R
@@ -7,10 +7,10 @@ test_that("data dimensions are correct", {
   correct_dimensions <- c(N, 3*(Tt+1) + 2) # Tt+1 periods of 3 variables (Lt,At,Yt), plus U0 and uid
 
   df_normal <- generate_data(N=N, Tt=Tt, Beta=Beta)
-  df_binomial <- generate_data(N=N, Tt=Tt, Beta=Beta, ylink='rbinom_logit')
+  df_binomial <- generate_data(N=N, Tt=Tt, Beta=Beta, ylink='rbinom_logit', binomial_n = round(runif(N, min=20, max=100)))
   df_hazard  <- generate_data(N=N, Tt=Tt, Beta=Beta, ylink='rbinom_logit_hazard')
 
   expect_equal(dim(df_normal), correct_dimensions)
-  expect_equal(dim(df_binomial), correct_dimensions)
+  expect_equal(dim(df_binomial), correct_dimensions + c(0, 1)) #has additional binomial_n column, but same # rows
   expect_equal(dim(df_hazard), correct_dimensions)
 })
