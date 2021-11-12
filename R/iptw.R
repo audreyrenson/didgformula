058b62d482fcf2d6e7ec77ce84c0a8a16ef54f1e
Audrey <audrey.o.renson@gmail.com>
2021-11-11 19:02:30 -0500

    iptw now supports aggregate binomial data

--- R/iptw.R
+++ R/iptw.R
@@ -3,20 +3,31 @@
 #' @param data Wide format data frame with one row per individual, and columns Lt, At, Yt for t = 0,1,...,Tt.
 #' @param rhs_formula chr. Formulas to use Format: '~L\{t\}+L\{t-1\}' etc.
 #' @param Tt int. Final period in dataset (t=0,1,...,Tt)
+#' @param ... Parameters passed to `glm`.
 #'
 #' @return list of models for period t=0,1,...,Tt, in that order.
 #' @export
 #'
 #' @examples
-fit_treatment_models <- function(data, rhs_formula, Tt) {
+fit_treatment_models <- function(data, rhs_formula, Tt, ...) {
   #returns a list of models
 
-  fit_one_model <- function(t) glm(formula = as.formula(glue::glue('A{t}', rhs_formula)),
-                                   family=binomial,
-                                   data=data,
-                                   subset=data[[glue::glue('A{t-1}')]] == 0)
 
-  return (sapply(1:Tt, fit_one_model, simplify=FALSE))
+  fit_one_model <- function(t) {
+    withCallingHandlers({
+      glm(formula = as.formula(glue::glue('A{t}', rhs_formula)),
+          family=binomial,
+          data=data,
+          subset=data[[glue::glue('A{t-1}')]] == 0,
+          ...)
+    }, warning = function(w) {
+      #the non-integer successes warning happens anytime you weight a binomial or other discrete glm, and isn't actually a problem
+      if (startsWith(conditionMessage(w), "non-integer #successes"))
+        invokeRestart("muffleWarning")
+    })
+  }
+
+  return (lapply(1:Tt, fit_one_model))
 
 }
 
@@ -52,9 +63,11 @@ calc_inclusion_indicators <- function(data, Tt) {
   sapply(1:Tt, function(t) 1 * (data[[glue::glue('A{t}')]] == 0), simplify = TRUE)
 }
 
-estimate_iptw <- function(ydiffs, weights,inclusion_indicators) {
+estimate_iptw <- function(ydiffs, weights, inclusion_indicators, binomial_n = 1) {
+  binomial_n = rep(1, nrow(ydiffs)) * binomial_n #get a column of 1's if this is not binomial aggregate data
+
   #returns a Tx1 vector of \hat\E[Y_t(\bar a) - Y_{t-1}(\bar a)], t=1,2,...,T
-  return (colMeans(inclusion_indicators * ydiffs * weights))
+  return (colSums(inclusion_indicators * ydiffs * weights) / sum(binomial_n))
 }
 
 #' Estimate the DID g-formula using inverse-probability-of-treatment-weights.
@@ -63,18 +76,24 @@ estimate_iptw <- function(ydiffs, weights,inclusion_indicators) {
 #' @param rhs_formula chr. Right-hand-side formula to use in predicting the treatment. Format: '~L\{t\}+L\{t-1\}' etc.
 #' @param Tt int. Final period in dataset (t=0,1,...,Tt)
 #' @param tibble logical. Should the results be returned as a tibble with columns (t, estimates) (TRUE) or a vector of just the estimates  (FALSE)?
+#' @param binomial_n int length nrow(data). Group sizes for aggregate binomial data.
 #'
 #' @return Estimates of E(Yt(a) - Yt-1(a)), in the form of a tibble or vector (dependening on argument tibble), for times t=1,2,...,Tt (in that order).
 #' @export
 #'
 #' @examples
-iptw_pipeline <- function(data, rhs_formula, Tt, tibble=TRUE) {
-  den_mods = fit_treatment_models(data, rhs_formula, Tt)
+iptw_pipeline <- function(data, rhs_formula, Tt, tibble=TRUE, binomial_n=1) {
+
+  if(!length(binomial_n) %in% c(1, nrow(data))) stop('binomial_n must be lenth 1 or nrow(data)')
+  binomial_n = binomial_n * rep(1, nrow(data)) # get a column of 1's if not aggregate binomial data
+
+  den_mods = fit_treatment_models(data, rhs_formula, Tt, weights = binomial_n / sum(binomial_n))
   den_preds = pred_treatment_models(data, den_mods)
   weights = calc_weights(den_preds)
   estimates = estimate_iptw(ydiffs = calc_ydiffs(data, Tt),
                             weights = weights,
-                            inclusion = calc_inclusion_indicators(data, Tt))
+                            inclusion = calc_inclusion_indicators(data, Tt),
+                            binomial_n = binomial_n)
   if(!tibble) {
     return (estimates)
   } else {
