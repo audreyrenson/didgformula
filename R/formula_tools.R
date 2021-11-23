403c86cd973bf0b18ec617120230b1df6afee6ab
Audrey <audrey.o.renson@gmail.com>
2021-11-23 15:24:58 -0500

    glue_formula() now implemented in ice functions.

--- R/formula_tools.R
+++ R/formula_tools.R
@@ -21,10 +21,10 @@ glue_formula = function(string, ...) {
   remove_negatives = function(glued_string) {
     #this is to get rid of references to variables before time 0
     glued_string %>%
-      str_remove('\\+[:alpha:]+-[:digit:]+') %>%#first remove those starting with +
-      str_remove('\\*[:alpha:]+-[:digit:]+') %>%#or a *
-      str_remove('\\:[:alpha:]+-[:digit:]+') %>%#etc.
-      str_remove('[:alpha:]+-[:digit:]+')
+      str_remove_all('\\+[:alpha:]+-[:digit:]+') %>%#first remove those starting with +
+      str_remove_all('\\*[:alpha:]+-[:digit:]+') %>%#or a *
+      str_remove_all('\\:[:alpha:]+-[:digit:]+') %>%#etc.
+      str_remove_all('[:alpha:]+-[:digit:]+')
   }
 
   positive_glued_trimmed_string = remove_negatives(glued_trimmed_string)
