db08b9dca37b686484dd51cf83b5d9610fa08104
Audrey <audrey.o.renson@gmail.com>
2021-12-05 10:45:48 -0500

    Changed OR estimator to marginalize before differencing

--- R/outcome_regression.R
+++ R/outcome_regression.R
@@ -32,7 +32,7 @@ get_replicate_data <- function(data, nreps, binomial_n=NULL) {
   data$binomial_n = binomial_n                   #include this column in the resampled data for later summarizing
   indices         = sample(1:nrow(data), size=nreps, replace=TRUE, prob=row_probs)
 
-  return ( data [ indices, c('uid','binomial_n', 'A0','L0','Y0')] )
+  return ( tibble::as_tibble (data [ indices, c('uid','binomial_n', 'A0','L0','Y0')] ) ) #replicate data needs to be a tibble because it will have matrix columns for Yt, Yt-1
 
 }
 
@@ -42,9 +42,9 @@ simulate_Lt <- function(replicate_data, cov_model) {
 
 simulate_Ydifft <- function(replicate_data,
                             outcome_models #list of 2 glm objects, for t-1 and t
-                            ) {
-  outcome_preds = sapply(outcome_models, predict, newdata=replicate_data, type="link", simplify = TRUE)
-  return ( outcome_preds[,2] - outcome_preds[,1] )
+) {
+  outcome_preds = sapply(outcome_models, predict, newdata=replicate_data, type="response", simplify = TRUE)
+  return ( outcome_preds ) #actually return a matrix of Yt, Yt-1 because we're going to marginalize first then take differences
 }
 
 simulate_fulldata <- function(replicate_data,
@@ -59,14 +59,22 @@ simulate_fulldata <- function(replicate_data,
   return ( replicate_data )
 }
 
-estimate_or <- function(simulated_data, Tt, binomial_n=NULL) {
+estimate_or <- function(simulated_data, Tt, link_fun, binomial_n=NULL) {
+
+  if(is.null(link_fun)) {
+    link_fun = function(x) x #built-in I() function does weird stuff
+  } else {
+    stopifnot(is.function(link_fun))
+  }
 
   if(is.null(binomial_n)) binomial_n = rep(1, nrow(simulated_data))
   freq_w = binomial_n / sum(binomial_n)  #so that weighted sum = weighted mean
 
   ydiff_names = sapply(1:Tt, function(t) glue('Ydiff{t}'))
 
-  return (colSums (freq_w * simulated_data[, ydiff_names]) )
+  weighted_ydiff_sums = sapply(ydiff_names, function(colname) colSums(freq_w * simulated_data[, colname])) #note that ydiffs are actually Nx2 matrices because we marginalize first then difference
+
+  return ( apply(weighted_ydiff_sums, 2, function(x) diff(link_fun(x))))
 }
 
 #' Outcome regression DID g-formula estimator
@@ -79,6 +87,7 @@ estimate_or <- function(simulated_data, Tt, binomial_n=NULL) {
 #' @param l_family `stats::family` object for the covariate models (which are fit using `glm`)
 #' @param Tt int. Max periods
 #' @param nreps int. Number of reps for monte-carlo simulation
+#' @param pt_link_fun function. The scale on which parallel trends is assumed (e.g., `qlogis` for logit scale). Default `NULL` for untransformed scale.
 #' @param tibble logical. Return results as tibble (TRUE) or vector (FALSE)?
 #'
 #' @return
@@ -89,32 +98,34 @@ or_pipeline <- function(data, yt_formula, ytmin1_formula,
                         l_formula,
                         y_family=gaussian,
                         l_family=binomial, Tt, nreps,
+                        pt_link_fun=NULL,
                         binomial_n=NULL, tibble=TRUE) {
 
-  cov_models = fit_covariate_models(data,
+  cov_models = fit_covariate_models(data=data,
                                     rhs_formula = l_formula,
                                     family=l_family,
                                     Tt=Tt,
                                     binomial_n = binomial_n)
 
-  out_models = fit_outcome_models(data,
+  out_models = fit_outcome_models(data=data,
                                   rhs_formula_t = yt_formula,
                                   rhs_formula_tmin1 = ytmin1_formula,
                                   family=y_family,
                                   Tt=Tt,
                                   binomial_n = binomial_n)
 
-  rep_data = get_replicate_data(data,
+  rep_data = get_replicate_data(data=data,
                                 nreps = nreps,
                                 binomial_n = binomial_n)
 
-  sim_data = simulate_fulldata(rep_data,
-                               cov_models,
-                               out_models,
-                               Tt)
+  sim_data = simulate_fulldata(replicate_data = rep_data,
+                               cov_models=cov_models,
+                               outcome_models=out_models,
+                               Tt=Tt)
 
-  or_estimates = estimate_or(sim_data,
-                             Tt,
+  or_estimates = estimate_or(simulated_data = sim_data,
+                             Tt=Tt,
+                             link_fun=pt_link_fun,
                              binomial_n=sim_data$binomial_n)
 
   if(!tibble) {
