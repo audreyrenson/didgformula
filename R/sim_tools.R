3d7dd9563577464d9a91d42ecede6f83b87d7190
Audrey <audrey.o.renson@gmail.com>
2021-12-07 10:42:20 -0500

    adding functions for evaluating implied densities from glm objects (mainly useful for IPTW)

--- R/sim_tools.R
+++ R/sim_tools.R
@@ -27,6 +27,68 @@ sim <- function(model, newdata, binomial_n=1) {
   simfun(eta, dispersion, n)
 
 }
+#' Evaluate data against the implied density from a fitted `glm` object based on possibly new covariate values, with automatic detection of family and link.
+#'
+#' @param x vector of values at which to evaluate the density
+#' @param model a fitted `glm` object
+#' @param newdata data frame with values of the covariates based on which to simulate the response, similar to `predict.glm`. nrow(newdata) must equal length(x).
+#' @param binomial_n int length nrow(newdata). If `model` was fit with `link=binomial`, you can optionally pass a vector of group sizes to simulate aggregate binomial data.
+#'
+#' @return A vector of length(x) of density estimates.
+#' @export
+#'
+#' @examples
+#' n  = 100
+#' df = tibble::tibble(x = rnorm(n), y = rpois(n, lambda = exp(x)))
+#' mod = glm(y~x, poisson('log'), df)
+#' dens_y = dens(df$y, mod, newdata=df)
+#'
+#' # equivalent to
+#' dpois(df$y, lambda = predict(mod, type='response'))
+#'
+dens <- function(x, model, newdata, binomial_n=1) {
+
+  if(length(x) != nrow(newdata)) stop('length(x) must equal nrow(newdata)')
+  if(!length(binomial_n) %in% c(1, length(x))) stop('binomial n must be length 1 or length(x)')
+
+  N = length(x)
+  n = binomial_n
+  eta = predict(model, newdata=newdata, type='link')
+  dispersion = summary(model)$dispersion
+  densfun = get_densfun(family(model))
+
+  densfun(x, eta, dispersion, n)
+}
+#' Generate a function that looks up a density in accordance with the specification in a `stats::family` object
+#'
+#' @description This function is useful for evaluating data against the implied density of a fitted `glm` object
+#'
+#' @param family an object of type `stats::family`
+#'
+#' @return A function with arguments `x` (data), `eta`, (linear predictor from `glm` object), `dispersion` (`summary(m)$dispersion` where `m` is a fitted `glm`), and  `n`, vector of group sizes (optional unless family=binomial)
+#' @export
+#'
+#' @examples
+#'
+#' fam = Gamma('log')
+#' densfun = get_densfun(fam)
+#' densfun(0.2, eta = 2, dispersion=0.5)
+#'
+#' # the above is equivalent to:
+#' dgamma(0.2, scale = exp(2), shape = 1/0.5)
+get_densfun <- function(family) {
+  function(x, eta, dispersion, n) {
+    mu = family$linkinv(eta)
+    stopifnot(length(x) == length(eta))
+
+    switch ( family$family,
+             'binomial' = dbinom(x, size=n, prob=mu),
+             'gaussian' = dnorm(x, mu, sqrt(dispersion)),
+             'Gamma' = dgamma(x, shape = 1/dispersion, scale = dispersion*mu),
+             'poisson' = dpois(x, lambda = mu),
+             'inverse.gaussian' = SuppDists::dinvGauss(x, nu=mu, lambda = 1/dispersion))
+  }
+}
 
 #' Generate a function that simulates data in accordance with the specification in a `stats::family` object
 #'
