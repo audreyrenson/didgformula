2e8cb910f521a4fd43bb6b8008e1c243a6ca8921
audreys work mac <audrey.o.renson@gmail.com>
2022-07-27 16:24:59 -0400

    Add 4-week case growth to stayathome2020. Also cleaning unnecessary libraries loaded in data-raw/stayathome2020.R

--- data-raw/stayathome2020.R
+++ data-raw/stayathome2020.R
@@ -5,24 +5,21 @@
 
 library(tidyverse)
 library(lubridate)
-library(splines)
-library(didgformula)
-theme_set(theme_minimal())
-
+library(glue)
 
 # load data ---------------------------------------------------------------
 path = 'data-raw/stayathome2020_rawfiles/'
 #from https://statepolicies.com/data/graphs/stay-at-home-order/
-lockdown_dates_raw <- read_csv(glue::glue("{path}COVID-19 US state policy database (CUSP) - Stay at Home.csv"))
+lockdown_dates_raw <- read_csv(glue("{path}COVID-19 US state policy database (CUSP) - Stay at Home.csv"))
 
 #from https://data.cdc.gov/NCHS/Weekly-Provisional-Counts-of-Deaths-by-State-and-S/muzy-jte6
-mortality_weekly_raw <- read_csv(glue::glue("{path}Weekly_Provisional_Counts_of_Deaths_by_State_and_Select_Causes__2020-2021.csv"))
+mortality_weekly_raw <- read_csv(glue("{path}Weekly_Provisional_Counts_of_Deaths_by_State_and_Select_Causes__2020-2021.csv"))
 
 #case data
-cases_daily <- read_csv(glue::glue("{path}time_series_covid19_confirmed_US.csv"))
+cases_daily_by_county <- read_csv(glue("{path}time_series_covid19_confirmed_US.csv"))
 
 #state denominators
-state_pop <- read_csv(glue::glue("{path}nhgis0042_ds244_20195_2019_state.csv"))
+state_pop <- read_csv(glue("{path}nhgis0042_ds244_20195_2019_state.csv"))
 
 
 # Merge data --------------------------------------------------------------
@@ -58,44 +55,35 @@ lockdown_weekly <- lockdown_dates_formatted %>%
   select(state, week_end, stayathome)
 
 
-cases_weekly <- cases_daily %>%
+cases_weekly <- cases_daily_by_county %>%
   select(state = Province_State, county = Admin2, `1/22/20`:`7/1/20`) %>%
-  pivot_longer(-state:-county, names_to = "date", values_to="daily_cumulative_cases_by_county") %>%
-  mutate(date = as.Date(date, format = "%m/%d/%y"),
-         week_end = date + 1 - wday(date + 1)) %>%
-  group_by(state, county, week_end) %>%
-  mutate(weekly_cumulative_cases_by_county = max(daily_cumulative_cases_by_county)) %>%
-  select(-daily_cumulative_cases_by_county, -date) %>%
-  unique() %>%
+  pivot_longer(-state:-county, names_to = "date", values_to="weekly_cumulative_cases_by_county") %>%
+  mutate(week_end = as.Date(date, format = "%m/%d/%y")) %>%
+  filter(wday(week_end) == 7) %>%
   group_by(state, week_end) %>%
   summarise(weekly_cumulative_cases_by_state = sum(weekly_cumulative_cases_by_county)) %>%
-  rename(cases_cumulative = weekly_cumulative_cases_by_state) %>%
-  mutate(cases_abs_growth = cases_cumulative - lag(cases_cumulative),
-         cases_perc_growth = 100 * (cases_abs_growth / cases_cumulative)) %>%
+  mutate(case_abs_growth_1wk = weekly_cumulative_cases_by_state - lag(weekly_cumulative_cases_by_state),
+         case_abs_growth_4wk = weekly_cumulative_cases_by_state - lag(weekly_cumulative_cases_by_state, n=4)) %>%
   ungroup()
 
 
 state_pop_formatted <- state_pop %>%
   select(state = STATE, pop = ALUBE001)
 
-full_weekly <- mortality_weekly %>%
+stayathome2020 <- mortality_weekly %>%
   inner_join(lockdown_weekly) %>%
   inner_join(cases_weekly) %>%
   inner_join(state_pop_formatted) %>%
   group_by(state) %>%
-  mutate(mort_rate_per100k = 1e5 * mort / pop,
-         case_abs_growth_per100k = 1e5 * cases_abs_growth / pop,
-         case_growth_lastweek = lag(case_abs_growth_per100k),
-         case_growth_twoweeksago = lag(case_abs_growth_per100k, n=2)) %>%
-  filter(!is.na(stayathome) & week_start >= as.Date("2020-04-05")) %>%
-  mutate(weeknum = week(week_end) - min( week(week_end))) %>%
-  ungroup()
+  mutate(case_abs_growth_1wk_per100k = lag(1e5 * case_abs_growth_1wk / pop), #these are lagged because we want the case growth in the week / 4 weeks previous
+         case_abs_growth_4wk_per100k = lag(1e5 * case_abs_growth_4wk / pop)) %>%
+  mutate(across(starts_with('case'), ~ifelse(.<0.001, 0.001, .))) %>% #this is only for rhode island, t=10, a=1, this avoids NAs from I(A=0) in IPW formula
+  ungroup() %>%
+  filter(!is.na(stayathome) & week_end >= as.Date("2020-04-05")) %>%
+  mutate(week = week(week_end) - min( week(week_end))) %>%
+  select(state, week, case_gr_1wk=case_abs_growth_1wk_per100k, case_gr_4wk=case_abs_growth_4wk_per100k, stayathome, mort, pop)
 
-stayathome2020 =full_weekly %>%
-  mutate(week      = weeknum,
-         case_gr = case_growth_lastweek %>%  ifelse(.<0.001, 0.001, .)) %>%#this is only for rhode island, t=10, a=1, this avoids NAs from I(A=0) in IPW formula
-  select(state, week, case_gr, stayathome, mort, pop)
-
-
+# # showing that the correlation is .91
+# with(stayathome2020, cbind(case_gr_1wk, case_gr_4wk)) %>% log() %>% cor()
 
 usethis::use_data(stayathome2020, overwrite = TRUE)
